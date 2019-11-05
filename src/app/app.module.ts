2f1d5387540382c506b6448039a5ab94d502ac80
SWTHOLD18 <47780943+SWTHOLD18@users.noreply.github.com>
2019-11-05 18:04:51 +0100

    Added firebase deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAgz0d2pZmPdfMhX9pRM5kUSXXPYHvG2PQ",
+    authDomain: "fitnessapp-88422.firebaseapp.com",
+    databaseURL: "https://fitnessapp-88422.firebaseio.com",
+    projectId: "fitnessapp-88422",
+    storageBucket: "fitnessapp-88422.appspot.com",
+    messagingSenderId: "307746942780",
+    appId: "1:307746942780:web:0b2aaa6d23d8b8a47cf839"
+  }
 };
