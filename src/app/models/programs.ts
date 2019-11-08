e64a1498d4d48f62fea5904e6f5812d5dd567faf
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-11-08 20:55:38 +0100

    testing stuff

--- src/app/models/programs.ts
+++ src/app/models/programs.ts
@@ -1,9 +1,6 @@
 import {Exercises} from "src/app/models/Exercises"
-import { ObjectUnsubscribedError } from 'rxjs';
-import { AbstractFormGroupDirective } from '@angular/forms';
 
 export interface Programs {
-    id: string;
     name: string;
     exercises: Exercises[];
 }
