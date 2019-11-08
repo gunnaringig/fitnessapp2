e3d1837e0fe1e9c028df30c46e38816b1c21efad
Gunnar Ingi Geirsson <gunnar.ingi.geirsson@gmail.com>
2019-11-08 23:26:27 +0100

    more visual changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-login',
@@ -13,5 +14,13 @@ export class LoginComponent {
 
   login() {
     this.auth.login();
+  }
+
+  loginEmail(){
+    //implement because poul the man needs to test
+    //And also tilføj to firebase
+    //could be just a new page, could also be a modal
+    // I added a modal just to show how it could look, 
+    //uses forms so should be easy enough to implement with firebase
   }
 }
